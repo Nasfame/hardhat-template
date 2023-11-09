a55577fab329e4e990e7bf31f97977f48ecd3d50
Hiro <laciferin@gmail.com>
2023-11-09 13:37:59 +0530

    init: from web3musketeers/web3.sub

---
+++ contracts/later-in-v3/UVx.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./web3subUpgradeable.interface.sol";
+
+// TODO: UVx
+contract UVx is Web3SubUpgradeable {
+    string public version = string.concat("uv", "x.0.0"); //TODO: update the version
+
+    constructor() {
+        // TODO: make it upgradeable later
+        string memory initMsg = string.concat(
+            "init:",
+            version,
+            Strings.toString(block.timestamp)
+        );
+        console.log(initMsg);
+    }
+
+    function subscribe(address contractAddress, address wallet) external {
+        // TODO: implement the logic
+        emit NewSubscription(contractAddress, wallet);
+    }
+
+    function unsubscribe(address contractAddress, address wallet) external {
+        // TODO: implement the logic
+        emit UnSubscribed(contractAddress, wallet);
+    }
+
+    function verify(
+        address contractAddress,
+        address wallet
+    ) external view returns (bool verified) {
+        require(contractAddress != address(0x0), "invalid contract address");
+        require(wallet != address(0x0), "invalid walletAddress");
+
+        // TODO: implement the logic
+
+        return verified;
+    }
+}
